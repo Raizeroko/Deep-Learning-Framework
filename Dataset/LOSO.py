813cd5aeb1e6f007d9197c5289b27a366a7511d9
Raizeroko <1464001349@qq.com>
2024-10-21 15:42:38 +0800

    Make LOSO.py more compatibility

--- Dataset/LOSO.py
+++ Dataset/LOSO.py
@@ -56,19 +56,24 @@ def SEED_Dataset_LOSOCV(input_dir, session, target_id):
     return train_dataset, test_dataset
 
 
-
-
-
-def DEAP_Dataset_LOSOCV(input_dir, session, target_id):
-    if session == 1:
-        data_dir = os.path.join(input_dir, 'Arousal')
-    elif session == 2:
-        data_dir = os.path.join(input_dir, 'Valence')
+def Dataset_LOSO(dataset, input_dir, session, subjects, trial, target_id):
+    data_dir = None
+    if dataset == 'SEED' or dataset == 'SEEDIV':
+        # SEED
+        data_dir = os.path.join(input_dir, f'Session{session}')
+    else:
+        # DEAP/DREAMER
+        if session == 1:
+            data_dir = os.path.join(input_dir, 'Arousal')
+        elif session == 2:
+            data_dir = os.path.join(input_dir, 'Valence')
+        elif session == 3:
+            data_dir = os.path.join(input_dir, 'Dominance')
 
     # 初始话空列表，用于存储从数据文件中读取的特征和标签
     feature_list = []
     label_list = []
-    for i in range(32):
+    for i in range(subjects):
         file_path = os.path.join(data_dir, f'subject{i+1}.mat')
         data = scio.loadmat(file_path)
         feature_trial = data['feature']
@@ -77,20 +82,20 @@ def DEAP_Dataset_LOSOCV(input_dir, session, target_id):
         label = None
 
         # 提取subject i 的feature和label
-        for i in range(40):
+        for i in range(trial):
             if feature == None:
                 feature = torch.tensor(feature_trial[f'trial{i+1}'][0][0])
-                label = torch.tensor(label_trial[f'trial{i+1}'][0][0])
+                label = torch.tensor(label_trial[f'trial{i+1}'][0][0]).reshape(-1)
             else:
                 feature = torch.cat((feature, torch.tensor(feature_trial[f'trial{i+1}'][0][0])), dim=0)
-                label = torch.cat((label, torch.tensor(label_trial[f'trial{i+1}'][0][0])), dim=0)
+                label = torch.cat((label, torch.tensor(label_trial[f'trial{i+1}'][0][0]).reshape(-1)), dim=0)
 
         feature = feature.permute(0, 2, 1).float()
-        label = label.reshape(-1).long()
+        label = label.long()
 
         feature_list.append(feature)
         label_list.append(label)
-        # label_list.append(one_hot_label)
+
 
     target_feature, target_label = feature_list[target_id-1], label_list[target_id-1]
     del feature_list[target_id-1]
@@ -113,13 +118,32 @@ def DEAP_Dataset_LOSOCV(input_dir, session, target_id):
 
 
 if __name__ == '__main__':
-    # input_dir = "/home/zwr/dataset/DEAP_Preprocessed"         #linux
-    input_dir = "E:/datasets/DEAP_Preprocessed"                 #windows
-    # input_dir = "E:/datasets/DEAP_DE_Preprocessed"
+    # input_dir = "E:/datasets/SEED_DE_Preprocessed_128"
+    # input_dir = "E:/datasets/DEAP_DE_Preprocessed_384"
+    # input_dir = "E:/datasets/DEAP_Time_Preprocessed_128"
+    # input_dir = "E:/datasets/DEAP_Preprocessed"
+    input_dir = "E:/datasets/DREAMER_Preprocessed"
+    # input_dir = "E:/datasets/SEED_DE_Preprocessed_128"
+    # input_dir = "E:/datasets/SEED_Time_Preprocessed_128"
 
-    subjects = 32
-    for session in range(1, 3):
-        for i in range(1, subjects+1):
+    dataset = input_dir.split('/')[-1].split('_')[0]
+    if dataset == 'SEED' or dataset == 'SEEDIV':
+        subjects = 15
+        trial = 15
+        kfold = 5
+    elif dataset == 'DEAP':
+        subjects = 32
+        trial = 40
+        kfold = 10
+    elif dataset == 'DREAMER':
+        subjects = 23
+        trial = 18
+        kfold = 6
+
+    for session in range(1, 4):
+        for i in range(1, subjects + 1):
+            train_dataset, test_dataset = Dataset_LOSO(dataset, input_dir, session, subjects, trial, i)
+            # train_dataset, test_dataset = Dataset_KFold_Sample(dataset, input_dir, session, i, trial, kfold, fold)
             print(f"session: {session}, subject: {i}")
-            DEAP_Dataset_LOSOCV(input_dir, session, i)
+
     print("success")
