244e467eaa5a0c845dc948b1898cea7523e7b2dd
Raizeroko <1464001349@qq.com>
2024-10-21 15:42:04 +0800

    Make KFold.py more compatibility

--- Dataset/KFold.py
+++ Dataset/KFold.py
@@ -5,7 +5,7 @@ import scipy.io as scio
 
 shuffle_indices = None
 
-def Dataset_KFold_Sample(dataset, input_dir, session, target_id, k, trial, fold, shuffle=True):
+def Dataset_KFold_Sample(dataset, input_dir, session, target_id, trial, k, fold, shuffle=True):
     global shuffle_indices
     data_dir = None
     if dataset == 'SEED' or dataset == 'SEEDIV':
@@ -77,7 +77,7 @@ def Dataset_KFold_Sample(dataset, input_dir, session, target_id, k, trial, fold,
     return train_dataset, test_dataset
 
 
-def Dataset_KFold_Trial(dataset, input_dir, session, target_id, k, trial, fold,  shuffle=True):
+def Dataset_KFold_Trial(dataset, input_dir, session, target_id, trial, k, fold,  shuffle=True):
     global shuffle_indices
     data_dir = None
     if dataset == 'SEED' or dataset == 'SEEDIV':
@@ -91,7 +91,6 @@ def Dataset_KFold_Trial(dataset, input_dir, session, target_id, k, trial, fold, 
             data_dir = os.path.join(input_dir, 'Valence')
         elif session == 3:
             data_dir = os.path.join(input_dir, 'Dominance')
-
     if data_dir == None:
         print('Dataset Error!')
 
@@ -145,87 +144,14 @@ def Dataset_KFold_Trial(dataset, input_dir, session, target_id, k, trial, fold, 
 
     return train_dataset, test_dataset
 
-
-
-
-def Dataset_KFold_USTrial(dataset, input_dir, session, target_id, k, trial, fold):
-    data_dir = None
-    if dataset == 'SEED' or dataset == 'SEEDIV':
-        # SEED
-        data_dir = os.path.join(input_dir, f'Session{session}')
-    else:
-        # DEAP/DREAMER
-        if session == 1:
-            data_dir = os.path.join(input_dir, 'Arousal')
-        elif session == 2:
-            data_dir = os.path.join(input_dir, 'Valence')
-        elif session == 3:
-            data_dir = os.path.join(input_dir, 'Dominance')
-
-    if data_dir == None:
-        print('Dataset Error!')
-
-    file_path = os.path.join(data_dir, f'subject{target_id}.mat')
-    data = scio.loadmat(file_path)
-    feature_trial = data['feature']
-    label_trial = data['label']
-
-    source_feature = None
-    source_label = None
-    target_feature = None
-    target_label = None
-
-    # 计算每个fold包含的trial数量
-    trials_per_fold = trial // k
-
-    # 提取特征和标签
-    for i in range(trial):
-        fold_index = i // trials_per_fold
-        if fold_index == fold:
-            if target_feature is None:
-                target_feature = torch.tensor(feature_trial[f'trial{i + 1}'][0][0])
-                target_label = torch.tensor(label_trial[f'trial{i + 1}'][0][0])
-            else:
-                target_feature = torch.cat((target_feature, torch.tensor(feature_trial[f'trial{i + 1}'][0][0])), dim=0)
-                target_label = torch.cat((target_label, torch.tensor(label_trial[f'trial{i + 1}'][0][0])), dim=0)
-        else:
-            if source_feature is None:
-                source_feature = torch.tensor(feature_trial[f'trial{i + 1}'][0][0])
-                source_label = torch.tensor(label_trial[f'trial{i + 1}'][0][0])
-            else:
-                source_feature = torch.cat((source_feature, torch.tensor(feature_trial[f'trial{i + 1}'][0][0])), dim=0)
-                source_label = torch.cat((source_label, torch.tensor(label_trial[f'trial{i + 1}'][0][0])), dim=0)
-
-    source_feature = source_feature.permute(0, 2, 1).float()
-    target_feature = target_feature.permute(0, 2, 1).float()
-    source_label = source_label.reshape(-1).long()
-    target_label = target_label.reshape(-1).long()
-
-    # 构建源域和目标域数据集
-    source_set = {'feature': source_feature, 'label': source_label}
-    target_set = {'feature': target_feature, 'label': target_label}
-
-    # 转为tensor数据集
-    train_dataset = Data.TensorDataset(source_set['feature'],
-                                       source_set['label'])
-    test_dataset = Data.TensorDataset(target_set['feature'],
-                                      target_set['label'])
-
-    return train_dataset, test_dataset
-
-
-
-
-
-
 if __name__ == '__main__':
-    # input_dir = "E:/datasets/SEED_DE_Preprocessed_128"
+    input_dir = "E:/datasets/SEED_DE_Preprocessed_128"
     # input_dir = "E:/datasets/DEAP_DE_Preprocessed_384"
     # input_dir = "E:/datasets/DEAP_Time_Preprocessed_128"
     # input_dir = "E:/datasets/DEAP_Preprocessed"
     # input_dir = "E:/datasets/DREAMER_Preprocessed"
     # input_dir = "E:/datasets/SEED_DE_Preprocessed_128"
-    input_dir = "E:/datasets/SEED_Time_Preprocessed_128"
+    # input_dir = "E:/datasets/SEED_Time_Preprocessed_128"
 
     dataset = input_dir.split('/')[-1].split('_')[0]
     if dataset == 'SEED' or dataset == 'SEEDIV':
@@ -244,10 +170,8 @@ if __name__ == '__main__':
     for session in range(1, 4):
         for i in range(1, subjects+1):
             for fold in range(kfold):
-                train_dataset, test_dataset = Dataset_KFold_Trial(dataset, input_dir, session, i, kfold, trial, fold)
-                # train_dataset, test_dataset = Dataset_KFold_Sample(dataset, input_dir, session, i, kfold, trial, fold)
-                print(f"session: {session}, subject: {i}, fold: {fold}")
-                # DEAP_Dataset_KFold_Sample(input_dir, session, i, k_fold, 18, fold)
-                # DEAP_DE_Dataset_KFold_Shuffle(input_dir, session, i, k_fold, fold)
+                train_dataset, test_dataset = Dataset_KFold_Trial(dataset, input_dir, session, i, trial, kfold, fold)
+                # train_dataset, test_dataset = Dataset_KFold_Sample(dataset, input_dir, session, i, trial, kfold, fold)
 
+                print(f"session: {session}, subject: {i}, fold: {fold}")
     print("success")
